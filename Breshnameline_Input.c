a676cd771f5e045631a5b4fc0b40aa4bff09f863
Bramheti Patil <103025887+c-40@users.noreply.github.com>
2022-10-26 12:18:49 +0530

    Update Breshnameline_Input.c

--- Breshnameline_Input.c
+++ Breshnameline_Input.c
@@ -1,32 +1,60 @@
-# include <stdio.h>
-# include <conio.h>
-# include <graphics.h>
-void main()
-{int dx,dy,x,y,p,x1,y1,x2,y2,gd,gm;
-
-clrscr();
-printf("\n\n\tEnter the co-ordinates of first point : ");
-scanf("%d %d",&x1,&y1);
-printf("\n\n\tEnter the co-ordinates of second point : ");
-scanf("%d %d",&x2,&y2);
-dx = (x2 - x1);
-dy = (y2 - y1);
-p = 2 * (dy) - (dx);
-x = x1;
-y = y1;
-detectgraph(&gd,&gm);
-initgraph(&gd,&gm,"C:\\TurboC3\\BGI");
-putpixel(x,y,WHITE);
-while(x <= x2)
-{if(p < 0)
-{x=x+1;
-y=y;
-p = p + 2 * (dy);
-}else
-{x=x+1;
-y=y+1;
-p = p + 2 * (dy - dx);
-}putpixel(x,y,WHITE);
-  }getch();  
-
-}
+#include<stdio.h>
+#include<graphics.h>
+#include<conio.h>
+#include<dos.h>
+#include<math.h>
+int main()
+   {
+    int gd = DETECT, gm,i,x,y,x1,y1,x2,y2,dx,dy; float d;
+     clrscr();
+    initgraph(&gd,&gm,"C:\\TurboC3\\BGI");
+     detectgraph(&gd,&gm);
+     printf("Enter the coordinate");
+     scanf("%d%d",&x1,&y1);
+     printf("Enter the coordinate");
+     scanf("%d%d",&x2,&y2);
+     dx=x2-x1;
+     dy=y2-y1;
+     x=x1;
+     y=y1;
+     if(abs(dx)>=abs(dy))
+     {
+     d=2*dy-dx;
+     while(x!=x2||y!=y2)
+     {putpixel(x,y,WHITE);
+     if(d<0)
+     {
+     x=x+1;
+     y=y;
+     d=d+2*dy;
+     }
+     else
+     {
+     x=x+1;
+     y=y+1;
+     d=d+2*dy-2*dx;
+     }
+     }
+     }
+     else
+     {
+     d=2*dx-dy;
+     while(x!=x2||y!=y2)
+     {putpixel(x,y,WHITE);
+     if(d<0)
+     {
+     x=x;
+     y=y+1;
+     d=d+2*dx;
+     }
+     else
+     {
+     x=x+1;
+     y=y+1;
+     d=d+2*dx-2*dy;
+     }
+     }
+     }
+     getch();
+     return 0;
+     }
